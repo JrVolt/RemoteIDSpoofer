36374e866395799123858f3e2364604495725839
JrVolt <andreapallinilavoro@gmail.com>
2025-07-21 00:10:25 +0200

    Dark theme baby

--- build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-arduino-cli compile --fqbn esp8266:esp8266:nodemcuv2 RemoteIDSpoofer/RemoteIDSpoofer.ino --clean
--- upload.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-arduino-cli upload --port /dev/ttyUSB0 --fqbn esp8266:esp8266:nodemcuv2 RemoteIDSpoofer/RemoteIDSpoofer.ino
